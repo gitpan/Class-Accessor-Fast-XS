6188e563717d7ef34f6609580883fab6af3db59d
Руслан У. Закиров <Ruslan.Zakirov@gmail.com>
2010-12-04 21:55:55 +0000

    Import of RUZ/Class-Accessor-Fast-XS-0.04 from CPAN.
    gitpan-cpan-distribution: Class-Accessor-Fast-XS
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         RUZ/Class-Accessor-Fast-XS-0.04.tar.gz
    gitpan-cpan-author:       RUZ
    gitpan-cpan-maturity:     released

--- t/bench.pl
+++ t/bench.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl
 
+use constant HAS_CXSAC => do { local $@; eval {require Class::XSAccessor::Compat; 1 } };
+
+if ( HAS_CXSAC ) {
+package CXSAC;
+our @ISA = 'Class::XSAccessor::Compat';
+__PACKAGE__->mk_ro_accessors('ro');
+__PACKAGE__->mk_wo_accessors('wo_one', 'wo_multi');
+__PACKAGE__->mk_accessors('both');
+}
 
 package CAF;
 
@@ -41,36 +50,41 @@ my @arr = (0..3);
 
 my $caf = CAF->new({ro => 'foo'});
 my $cafxs = CAFXS->new({ro => 'foo'});
+my $cxsac = CXSAC->new({ro => 'foo'});
+$cxsac = CXSAC->new({ro => 'foo'}) if HAS_CXSAC;
 my $dummy = bless { ro => 'foo' }, 'Dummy';
 my $dummy_sub = bless { ro => 'foo' }, 'DummySub';
 
-
-my $t = -1;
+my $t = shift || -1;
 
 cmpthese($t, {
 	get_caf => sub { $caf->ro },
-	get_caf_xs => sub { $cafxs->ro },
+	get_cafxs => sub { $cafxs->ro },
+	HAS_CXSAC? (get_cxsac => sub { $cxsac->ro }) : (),
 	get_dummy_ha => sub { $dummy->{'ro'} },
 	get_dummy_sub => sub { $dummy_sub->ro },
 });
 
 cmpthese($t, {
 	set_one_caf => sub { $caf->wo_one('new') },
-	set_one_caf_xs => sub { $cafxs->wo_one('new') },
+	set_one_cafxs => sub { $cafxs->wo_one('new') },
+	HAS_CXSAC? (set_one_cxsac => sub { $cxsac->wo_one('new') }) : (),
 	set_one_dummy_ha => sub { $dummy->{'wo_one'} = 'new' },
 	set_one_dummy_sub => sub { $dummy_sub->wo_one('new') },
 });
 
 cmpthese($t, {
 	set_multi_caf => sub { $caf->wo_multi('foo', 'bar') },
-	set_multi_caf_xs => sub { $cafxs->wo_multi('foo', 'bar') },
+	set_multi_cafxs => sub { $cafxs->wo_multi('foo', 'bar') },
+	HAS_CXSAC? (set_multi_cxsac => sub { $cxsac->wo_multi('foo', 'bar') }) : (),
 	set_multi_dummy_ha => sub { $dummy->{'wo_multi'} = ['foo', 'bar'] },
 	set_multi_dummy_sub => sub { $dummy_sub->wo_multi('foo', 'bar') },
 });
 
 cmpthese($t, {
 	mix_caf => sub { $caf->both('new'); my $v = $caf->both; $v = $caf->both; },
-	mix_caf_xs => sub { $cafxs->both('new'); my $v = $cafxs->both; $v = $cafxs->both; },
+	mix_cafxs => sub { $cafxs->both('new'); my $v = $cafxs->both; $v = $cafxs->both; },
+	HAS_CXSAC? (mix_cxsac => sub { $cxsac->both('new'); my $v = $cxsac->both; $v = $cxsac->both; }) : (),
 	mix_dummy_ha => sub { $dummy->{'both'} = 'new'; my $v = $dummy->{'both'}; $v = $dummy->{'both'} },
 	mix_dummy_sub => sub { $dummy_sub->both('new'); my $v = $dummy_sub->both; $v = $dummy_sub->both },
 });
